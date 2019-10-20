e8ea18b5eb14faa4e3bea281c3f277b3cb57c8f1
Unknown <65696501i26@gmail.com>
2019-10-20 21:34:37 +0300

    1.7: рисование узоров на Canvas 1
    no problems

---
+++ FotItSchool/app/src/main/java/com/example/fotitschool/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fotitschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyDraw(this));
+    }
+}
