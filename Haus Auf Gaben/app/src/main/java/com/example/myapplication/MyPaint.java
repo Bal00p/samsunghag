71886acaaf8ec1428ef2d975e897cb092b2dbc4d
Unknown <65696501i26@gmail.com>
2019-10-21 23:09:07 +0300

    1.8.8: Графики функций 1
    no proplems

--- Haus Auf Gaben/app/src/main/java/com/example/myapplication/MyPaint.java
+++ Haus Auf Gaben/app/src/main/java/com/example/myapplication/MyPaint.java
@@ -1,38 +1,76 @@
 package com.example.myapplication;
 
 import android.content.Context;
-import android.graphics.*;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Shader;
 import android.view.View;
 
 public class MyPaint extends View {
+    double xe, ye, w;
+    double x, y, h = 0.1;
+    int k = 100;
+    //double xmin = -2 * Math.PI, xmax = 2 * Math.PI;
+    double xmin = -4, xmax = 4;
+    @Override
+    protected void onDraw(Canvas canvas) {
 
-    int N = 10; // количество шариков
-    float[] x  = new float[N];
-    float[] y  = new float[N];
-    float[] vx = new float[N];
-    float[] vy = new float[N];
+        int x0 = this.getWidth() / 2;
+        int y0 = this.getHeight() / 2;
 
-    public MyPaint(Context context) {
-        super(context);
-        for (int i = 0; i < N; i++){
-            x[i] = (float)(Math.random() * 500);
-            y[i] = (float)(Math.random() * 500);
-            vx[i] = (float)(Math.random() * 6 - 3);
-            vy[i] = (float)(Math.random() * 6 - 3);
+        Paint paint = new Paint();
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setTextSize(30.0f);
+
+        canvas.drawLine(x0, 0, x0, this.getHeight(), paint);
+        canvas.drawText("Y", this.getWidth() / 2 + 10, 35, paint);
+        canvas.drawLine(0, y0, this.getWidth(), y0, paint);
+        canvas.drawText("X", this.getWidth() - 50, this.getHeight() / 2 + 50, paint);
+
+        Path path = new Path();
+        boolean first = true;
+        paint.setColor(Color.BLUE);
+        for (x = xmin; x < xmax; x += h) {
+
+            y=Math.abs(x);
+            //y = Math.pow(x,4);
+            // y=Math.tan(x);
+            // y = 3 * Math.sin(x);
+            xe = x0 + k * x;
+            ye = y0 - k * y;
+            if (first) {
+                path.moveTo((float) xe, (float) ye);
+                first = false;
+            } else {
+                path.lineTo((float) xe, (float) ye);
+            }
+            canvas.drawPath(path, paint);
+        }
+        Path path2 = new Path();
+        first = true;
+        paint.setColor(Color.RED);
+        for (x = xmin; x < xmax; x += h) {
+            y=-x*x+3;
+            // y=Math.tan(x);
+            // y = 3 * Math.sin(x);
+            xe = x0 + k * x;
+            ye = y0 - k * y;
+            if (first) {
+                path2.moveTo((float) xe, (float) ye);
+                first = false;
+            }
+            else
+            {
+                path2.lineTo((float) xe, (float) ye);
+            }
+            canvas.drawPath(path2, paint);
         }
     }
-
-    Paint paint = new Paint();
-    @Override
-
-    protected void onDraw(Canvas canvas) {
-        for (int i = 0; i < N; i++) {
-            canvas.drawCircle(x[i], y[i], 20, paint);
-        }
-        for (int i = 0; i < N; i++) {
-            x[i] += vx[i];
-            y[i] += vy[i];
-        }
-        invalidate();
+    public MyPaint(Context context) {
+        super(context);
     }
 }
