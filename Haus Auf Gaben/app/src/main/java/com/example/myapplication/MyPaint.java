2cc933ba85fed7e3d095f40da8349340f8bc1550
Unknown <65696501i26@gmail.com>
2019-10-21 23:00:29 +0300

    1.8: простейшая анимация 2
    no problems

--- Haus Auf Gaben/app/src/main/java/com/example/myapplication/MyPaint.java
+++ Haus Auf Gaben/app/src/main/java/com/example/myapplication/MyPaint.java
@@ -6,19 +6,33 @@ import android.view.View;
 
 public class MyPaint extends View {
 
-    MyPaint(Context context) {
+    int N = 10; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+
+    public MyPaint(Context context) {
         super(context);
+        for (int i = 0; i < N; i++){
+            x[i] = (float)(Math.random() * 500);
+            y[i] = (float)(Math.random() * 500);
+            vx[i] = (float)(Math.random() * 6 - 3);
+            vy[i] = (float)(Math.random() * 6 - 3);
+        }
     }
+
     Paint paint = new Paint();
-    float x = 0;
-    long lastTime = System.currentTimeMillis();
     @Override
 
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(x, 300, 20, paint);
-        long nowTime = System.currentTimeMillis();
-        x += 0.9f * (nowTime - lastTime);
-        lastTime = nowTime;
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
         invalidate();
     }
 }
