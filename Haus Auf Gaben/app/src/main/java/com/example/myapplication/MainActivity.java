bf2d1b4f2f438666821e761d5f60eabed1ef1e35
Unknown <65696501i26@gmail.com>
2019-10-31 20:15:46 +0300

    1.11.5: клетчатое поле в Android
    ПРОБЛЕМА: выходит за массив, не вижу, где

--- Haus Auf Gaben/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Haus Auf Gaben/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.GridLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener,
+        OnLongClickListener {
+
+    private int HEIGHT = 15;
+    private int WIDTH = 10;
+
+
+    private Button[][] cells;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyPaint(this));
+        setContentView(R.layout.cells);
+        makeCells();
+        generate();
     }
+
+
+    void generate() {
+
+        int num=1;
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                cells[i][j].setText(num + "");
+                num++;
+            }
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++)
+                if (Math.random() >= 0.5)
+                {
+                    cells[i][j].setBackgroundColor(Color.YELLOW);
+                }
+    }
+
+
+    @Override
+    public boolean onLongClick(View v) {
+
+
+        return false;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        Button tappedCell = (Button) v;
+        //Получаем координтаты нажатой клетки
+        int tappedX = getX(tappedCell);
+        int tappedY = getY(tappedCell);
+        for (int x = 0; x < HEIGHT; x++)
+        {
+            cells[tappedY][x].setBackgroundColor(Color.RED);
+        }
+        for (int y = 0; y < WIDTH; y++)
+        {
+            cells[y][tappedX].setBackgroundColor(Color.RED);
+        }
+
+    }
+
+
+    /*
+     * NOT FOR THE BEGINNERS
+     * ==================================================
+     */
+
+
+    int getX(View v) {
+        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
+    }
+
+    int getY(View v) {
+        return Integer.parseInt(((String) v.getTag()).split(",")[0]);
+    }
+
+
+    void makeCells() {
+        cells = new Button[HEIGHT][WIDTH];
+        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
+        cellsLayout.removeAllViews();
+        cellsLayout.setColumnCount(HEIGHT);
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++) {
+                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
+                cells[i][j].setOnClickListener(this);
+                cells[i][j].setOnLongClickListener(this);
+                cells[i][j].setTag(i + "," + j);
+                cellsLayout.addView(cells[i][j]);
+            }
+    }
+
 }
--- Haus Auf Gaben/app/src/main/java/com/example/myapplication/MyPaint.java
+++ Haus Auf Gaben/app/src/main/java/com/example/myapplication/MyPaint.java
@@ -1,91 +1,15 @@
-package com.example.myapplication;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.util.AttributeSet;
-import android.view.View;
 
-public class MyPaint extends View {
-
-    int N = 50; // количество шариков
-    float[] x  = new float[N];
-    float[] y  = new float[N];
-    float[] vx = new float[N];
-    float[] vy = new float[N];
-    int[] Red = new int[N];
-    int[] Green = new int[N];
-    int[] Blue = new int[N];
-//    public MyPaint(Context context) {
-//        super(context);
-//        for (int i = 0; i < N; i++) {
-//            x[i] = (float) (Math.random() * 500);
-//            y[i] = (float) (Math.random() * 500);
-//            vx[i] = (float) (Math.random() * 6 - 3);
-//            vy[i] = (float) (Math.random() * 6 - 3);
-//        }
+// package com.example.myapplication;
+//
+//         import androidx.appcompat.app.AppCompatActivity;
+//         import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(new MyPaint(this));
 //    }
-
-    float rand(float min , float max){
-        return (float)(Math.random() * (max - min + 1)) + min;
-    }
-
-    void fillRandom(float[] array , float min, float max){
-        for (int i = 0; i < array.length; i++){
-            array[i] = rand (min, max);
-        }
-    }
-
-    void fillArrayRandom(int[] a, int min, int max) {
-        for (int i = 0; i < a.length; i++) {
-            a[i] = (int) (Math.random() * (max - min + 1)) + min;
-        }
-    }
-
-    void colorRand(){
-            fillArrayRandom(Red, 50, 255);
-            fillArrayRandom(Green, 50, 255);
-            fillArrayRandom(Blue, 50, 255);
-    }
-
-    public MyPaint(Context context) {
-        super(context);
-        fillRandom(x, 100, 500);
-        fillRandom(y, 100, 500);
-        fillRandom(vx, -3, 3);
-        fillRandom(vy, -3, 3);
-    }
-
-    void add(float[] array , float[] values){
-        for (int i = 0; i < array.length; i++){
-            array[i] += values[i];
-        }
-    }
-
-    Paint paint = new Paint();
-
-
-
-    void drawBalls(Canvas canvas){
-        colorRand();
-        for (int i = 0; i < N; i++) {
-            canvas.drawCircle(x[i], y[i], 20, paint);
-            paint.setColor(Color.argb(255, Red[i],Green[i],Blue[i]));
-        }
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        // отрисовываем все шарики
-        drawBalls (canvas);
-        // готовим массивы x и у для следущего кадра
-        add(x, vx);
-        add(y, vy);
-        //запрашиваем перерисовку
-        invalidate();
-    }
-}
+//}
